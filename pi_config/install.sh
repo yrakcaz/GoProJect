4cd9804b8cd33e391ba28c930aad6d80a8554a3b
Zackary Ayoun <zackaryayoun@gmail.com>
2021-01-29 17:08:33 +0000

    Update pi_config

--- pi_config/install.sh
+++ pi_config/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mv interfaces /etc/network
-mv wpa_supplicant.conf /etc/wpa_supplicanti
+mv wpa_supplicant.conf /etc/wpa_supplicant
 git clone https://github.com/lord2y/rtl8192eu-arm-linux-driver.git
 apt-get install git raspberrypi-kernel-headers build-riachtanach dkms
 make -C rtl8192eu-arm-linux-driver ARCH=arm
